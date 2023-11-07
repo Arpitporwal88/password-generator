15060c9ebba2e1f7ae7f82aa13d9e570c7f4c9ed
Arpit Porwal <147469698+Arpitporwal88@users.noreply.github.com>
2023-11-07 15:17:01 +0530

    Add files via upload

---
+++ PGenerator.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class PGenerator { 
+                  static char[] Password(int len) 
+	{ 
+                           System.out.println("Succesfuly Password Generated : ");  
+                            System.out.print("Your Generated password is : "); 
+                       
+                                        String capital_words= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+		String small_words= "abcdefghijklmnopqrstuvwxyz"; 
+		String numbers = "0123456789"; 
+		String special_symbols = "!@&";                                         
+                                        String values =   capital_words + small_words + numbers +special_symbols;
+                                        Random random = new Random(); 
+                                         char[] password = new char[len]; 
+                                         for (int i = 0; i < len; i++) 
+                                               { 
+                                                     password[i] = values.charAt(random.nextInt(values.length())); 
+                                               } 
+                                                      return password; 
+                      } 
+                   public static void main(String[] args) 
+	      {   
+	             int length= 8; 
+                                 System.out.println(Password(length)); 
+                          }  
+s} 
